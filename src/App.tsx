12f3c4ca414c2780a7a3eb487674943aab667bd2
tchomicz <tchomicz@pgs-soft.com>
2022-09-02 12:23:32 +0200

    RB-2 translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import { useTranslation } from 'react-i18next';
+
+const lngs = {
+  en: { nativeName: 'English' },
+  pl: { nativeName: 'Polski' }
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { t, i18n } = useTranslation(['translation']);
 
   return (
     <div className="App">
+      <div>
+        {Object.keys(lngs).map((lng: string) => (
+          <button key={lng} style={{ fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal' }} type="submit" onClick={() => i18n.changeLanguage(lng)}>
+            {lngs[lng].nativeName}
+          </button>
+        ))}
+      </div>
 
-      <h1>React</h1>
+      <h1>{t('React')}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
