403f5dfede41c10dadd19ee445c054993dc9bf07
tchomicz <tchomicz@pgs-soft.com>
2022-09-07 10:14:56 +0200

    RB-4 Template class

--- src/templates/page-template/page-template.tsx
+++ src/templates/page-template/page-template.tsx
@@ -1,14 +1,24 @@
 import { Box, Container } from '@mui/material';
-import { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import NavBar from '../../components/navbar/navbar';
 import { PageTemplateProps } from './page-template.interface';
 
 const PageTemplate: FC<PageTemplateProps> = ({ children }) => {
+  const [state, setState] = useState<null | any>(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => {
+        setState({ todo: json });
+      });
+  });
   return (
     <Box>
       <NavBar />
       <Container>{children}</Container>
+      <p>title : {state?.todo?.title}</p>
     </Box>
   );
 };
